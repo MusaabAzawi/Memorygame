89e0c72ba0e4bd440b4f078d484a2b3b656f547b
Musaab Azawi <musaab.azawi@student.nhlstenden.com>
2018-10-02 16:00:35 +0200

    ver 0.2

--- Memorygame/Program.cs
+++ Memorygame/Program.cs
@@ -10,6 +10,10 @@ namespace Memorygame
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("dit is een test");
+            Console.ReadLine();
         }
+        
     }
 }

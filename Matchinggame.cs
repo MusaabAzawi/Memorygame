41b5db00f7fbc0e95d5f5890f177ae745ce39f77
Musaab Azawi <musaab.azawi@student.nhlstenden.com>
2018-10-13 16:23:33 +0200

    images toegevoegd aan list

---
+++ Matchinggame.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryGame1._0
+{
+    public partial class Matchinggame : Form
+    {
+        bool allowClick = false;
+        PictureBox firstGuess; 
+        Random rnd = new Random();
+        Timer clickTimer = new Timer();
+        int time = 60;
+        Timer timer = new Timer { Interval = 1000 };
+
+        public Matchinggame()
+        {
+            InitializeComponent();
+        }
+
+        private PictureBox[] pictureBoxes
+        {
+            get { return Controls.OfType<PictureBox>().ToArray(); }
+        }
+        private static IEnumerable<Image> images
+        {
+            get
+            {
+                return new Image[]
+                {
+                    Afbeelding.achterkant_kaartjes,
+                    Afbeelding.pic1,
+                    Afbeelding.pic10,
+                    Afbeelding.pic11,
+                    Afbeelding.pic12,
+                    Afbeelding.pic13,
+                    Afbeelding.pic14,
+                    Afbeelding.pic15,
+                    Afbeelding.pic16,
+                    Afbeelding.pic2,
+                    Afbeelding.pic3,
+                    Afbeelding.pic4,
+                    Afbeelding.pic5,
+                    Afbeelding.pic6,
+                    Afbeelding.pic8,
+                    Afbeelding.pic7,
+                    Afbeelding.pic9,
+
+                };
+            }
+        }
+
+        private void startGameTimer()
+        {
+            timer.Start();
+            timer.Tick += delegate
+            {
+                time--;
+                if (time < 0)
+                {
+                    timer.Stop();
+                    MessageBox.Show("Tijd is afgelopen");
+                    ResetImages();
+                }
+
+                var ssTime = TimeSpan.FromSeconds(time);
+                label1.Text = "00:" + time.ToString();
+            };
+        }
+
+        private void ResetImages()
+        {
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Tag = null;
+                pic.Visible = true;
+            }
+        
+        HideImages();
+        setRandomImages();
+        time = 60;
+        timer.Start();
+    }
+
+    private void HideImages()
+    {
+        foreach(var pic in pictureBoxes)
+            {
+                pic.Image = Afbeelding.achterkant_kaartjes;
+            }
+    }
+
+        private PictureBox getFreeSlot()
+        {
+            int num;
+
+            do
+            {
+                num = rnd.Next(0, pictureBoxes.Count());
+            }
+            while (pictureBoxes[num].Tag != null);
+            return pictureBoxes[num];
+        }
+    
+        private void setRandomImages()
+        {
+            foreach(var image in images)
+            {
+                getFreeSlot().Tag = image;
+                getFreeSlot().Tag = image;
+            }
+        }
+
+        private void CLICKTIMER_TICK(object sender, EventArgs e)
+        {
+            HideImages();
+            allowClick = true;
+            clickTimer.Stop();
+        }
+
+        private void clickImage(object sender, EventArgs e)
+        {
+            if (!allowClick) return;
+
+            var pic = (PictureBox)sender;
+
+            if (firstGuess == null)
+            {
+                firstGuess = pic;
+                pic.Image = (Image)pic.Tag;
+                return;
+            }
+            pic.Image = (Image)pic.Tag;
+
+            if (pic.Image == firstGuess.Image && pic != firstGuess)
+            {
+                pic.Visible = firstGuess.Visible = false;
+                {
+                    firstGuess = pic;
+                }
+                HideImages();
+            }
+            else
+            {
+                allowClick = false;
+                clickTimer.Start();
+            }
+
+            firstGuess = null;
+            if (pictureBoxes.Any(p => p.Visible)) return;
+            MessageBox.Show("je bent gewonnen! probeer nog een keer");
+            ResetImages();
+        }
+        private void startGame(object sender, EventArgs e)
+        {
+            allowClick = true;
+            setRandomImages();
+            HideImages();
+            startGameTimer();
+            clickTimer.Interval = 1000;
+            clickTimer.Tick += CLICKTIMER_TICK;
+            button1.Enabled = false;
+        }
+        }
+    }
--- SpeelScherm.xaml.cs
+++ SpeelScherm.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,6 +12,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.Resources;
+using System.Globalization;
+
 
 namespace MemoryGame1._0
 {
@@ -19,10 +24,41 @@ namespace MemoryGame1._0
     /// </summary>
     public partial class SpeelScherm : Window
     {
+        private bool allowClick = false; // play again or not
+        private Afbeelding _firstGuess;
+        private readonly Random _random = new Random(); // selecet  arandom value from x and u list and assign a new location to each card
+        private readonly Timer _clickTimer = new Timer();
+        int tick = 60; // scores
+        readonly Timer timer = new Timer { Interval = 1000 };
+        //List<int> X = new List<int>(); //X Values of each picturebox
+        //List<int> Y = new List<int>();// Y values of each picturebox
+        // gebruik dit object om random icon te plaatsen in de grid
         public SpeelScherm()
         {
-
             InitializeComponent();
+            SetRandomImages();
+            HideImages();
+            StartGameTimer();
+            _clickTimer.Interval = 1000l;
+            _clickTimer.Tick += _clickTimer_Tick;
         }
+
+        private static Afbeelding[] afbeeldings
+        {
+            get { return Controls.typeof<Afbeelding>().ToArray(); }
+        }
+        //ResourceManager Afbeelding = new ResourceManager(typeof(Afbeelding));
+        //ResourceSet resourceSet = Properties.Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentCulture, true, true);
+        private void SpeelScherm_Load(object sender, EventArgs e)
+        {
+            //foreach (Afbeelding afbeelding  in Resources)
+            //{
+            //   // (Bitmap)Properties.Resources.ResourceManager.GetObject(list[2])
+            //  // Afbeelding.Image = ResourceManager;
+            //}
+        }
+            
+        
     }
+        
 }
